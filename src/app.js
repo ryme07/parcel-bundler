24d9557b2b2c55d2e8141833c4eccc46b2c77ba4
ryme07 <remydelrieu@hotmail.fr>
2021-02-11 15:58:41 +0100

    feat: three scene done with rotation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,43 @@
-import { hello } from "./js/hello";
+import animate from "./js/animate";
+import createLight from "./js/createLight";
+import createRenderer from "./js/createRenderer";
+import createCamera from "./js/createCamera";
+import createCube from "./js/createCube";
+import createScene from "./js/createScene";
 import "./styles/main.scss";
 
-const init = () => hello();
+const renderer = createRenderer();
+const scene = createScene();
+const camera = createCamera();
 
-init();
+const cubes = {
+  pink: createCube({ color: 0xff00ce, x: -1, y: -1, z: 2 }),
+  purple: createCube({ color: 0x9300fb, x: 1, y: -1 }),
+  blue: createCube({ color: 0x0065d9, x: 1, y: 1, z: -2 }),
+  cyan: createCube({ color: 0x00d7d0, x: -1, y: 1 }),
+};
+
+const light = createLight();
+
+for (const cube of Object.values(cubes)) {
+  scene.add(cube);
+}
+scene.add(light);
+
+const updateCube = () => {
+  cubes.purple.rotation.x += 0.01;
+  cubes.cyan.rotation.y += 0.05;
+};
+
+animate(() => {
+  updateCube();
+  renderer.render(scene, camera);
+});
+
+// RESIZE SCENE
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  camera.updateProjectionMatrix();
+});
--- src/js/createCube.js
+++ src/js/createCube.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 
-export default function createCube({ color, x, y }) {
+export default function createCube({ color, x, y, z }) {
   const geometry = new THREE.BoxGeometry();
   const material = new THREE.MeshLambertMaterial({ color });
   const cube = new THREE.Mesh(geometry, material);
-  cube.position.set(x, y, 0);
+  cube.position.set(x, y, z, 0);
 
   return cube;
 }
