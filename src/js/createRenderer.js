a356fe92ddbe6edbcdffcd0c60baab34f9de04fa
ryme07 <remydelrieu@hotmail.fr>
2021-02-11 16:00:43 +0100

    fix: change name of variable

--- src/js/createRenderer.js
+++ src/js/createRenderer.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 
 export default function createRenderer() {
-  const root = document.getElementById("app");
+  const canvas = document.getElementById("app");
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  root.appendChild(renderer.domElement);
+  canvas.appendChild(renderer.domElement);
   return renderer;
 }
